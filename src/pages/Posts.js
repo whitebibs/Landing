ffecfa882e3679fbd703b680637b723d7dfdf38b
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-03-16 10:34:19 +0100

    pagination post

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -5,6 +5,13 @@ const {BASE_API_URL} = Constants;
 
 export const Posts = () => {
 const [data, error, loading] = useAxios(`${BASE_API_URL}/posts`)
+const [currentPage, setCurrentPage] = useState(1); //partendo da pagina 1
+const postsPerPage= 10; // 10 perchè 100 
+const lastPosts = currentPage * postsPerPage;
+const firstPosts= lastPosts - postsPerPage; 
+
+const pageCount = Math.ceil(data.length / postsPerPage); //arrotondo per il nr delle pagien
+const currentPosts = data.slice(firstPosts, lastPosts); //cosi ci viene restituito un nuovo array con gli elementi selezionati, dall'inizio alla fine
 
 if(error) {
     return <h3>Error: {error.message}</h3>
@@ -13,7 +20,16 @@ if(error) {
   }
 
 
-
+const handleClick = (e) => {
+  const {step} = e.target.dataset;
+  if(
+    (currentPage === 1 && step > 0 ) || 
+    (currentPage === pageCount && step < 0) ||
+    (currentPage > 1 && currentPage < pageCount) 
+  ){
+    setCurrentPage(currentPage + Number(step))
+  }
+}
 const postsList = currentPosts.map((post)=> (
   <li key={post.id}  className="p-3 m-2 font-extrabold border-2 border-black border-solid">
     <div>
@@ -29,7 +45,10 @@ return(
         <h1 className="flex justify-center mb-2 text-2xl font-bold dark:text-white md:text-5xl lg:text-6xl">Posts</h1>
         <ul>
           {postsList}
-          
+          <button onClick={handleClick} data-step={-1}>←
+          </button>
+          <span> Page {currentPage} of {pageCount}</span>
+          <button onClick={handleClick} data-step={1}> →</button>
         </ul>
         </div>
     )
