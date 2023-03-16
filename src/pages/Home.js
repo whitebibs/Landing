682f4d9e58c00b852b37ef3d84c7cf2362a4de84
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-03-16 10:31:03 +0100

    landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,41 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+export const Home = () => {
+    return (
+        <div>
+            <h1 className="flex items-center justify-center mt-3 text-2xl font-bold  dark:text-white md:text-5xl lg:text-6xl h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500">Home<svg class="h-12 w-12 flex items-center mt-2 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </h1>
+            <div className="flex justify-center mt-28">
+                <p className="w-6/12 p-8 font-sans font-extrabold ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+                    sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+                    recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
+                    minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit</p>
+            </div>
+            <footer className="container flex flex-col items-center justify-between p-6 mx-auto mt-10 md:">
+                <div className="flex">
+                    <a href="#" className="flex items-center mx-3 duration-150 hover:opacity-80"><svg class="h-4 w-4 m-1 text-blue-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                        About us</a>
+                    <a href="#" className="flex items-center mx-3 duration-150 hover:opacity-80"><svg class="h-4 w-4 m-1 text-green-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <rect x="4" y="13" rx="2" width="4" height="6" />  <rect x="16" y="13" rx="2" width="4" height="6" />  <path d="M4 15v-3a8 8 0 0 1 16 0v3" />  <path d="M18 19a6 3 0 0 1 -6 3" /></svg>Contact</a>
+                    <a href="#" className="flex items-center mx-3 duration-150 hover:opacity-80"><svg class="h-4 w-4 m-1 text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+                    </svg>
+                        Privacy</a>
+                </div>
+            </footer>
+        </div>
 
+    )
+
+}
  
 /*https://www.tailwindtoolbox.com/icons */
