4cabbdb5049e7d61dfeca6b0a86e34120bb2bbf8
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-03-16 10:33:57 +0100

    rendering posts

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,4 +1,39 @@
+import useAxios from "../hook/useAxios"
+import { Constants } from "../constants"
+import { useState } from "react";
+const {BASE_API_URL} = Constants;
 
+export const Posts = () => {
+const [data, error, loading] = useAxios(`${BASE_API_URL}/posts`)
+
+if(error) {
+    return <h3>Error: {error.message}</h3>
+  }if (loading) {
+    return <h3>Loading...</h3>
+  }
+
+
+
+const postsList = currentPosts.map((post)=> (
+  <li key={post.id}  className="p-3 m-2 font-extrabold border-2 border-black border-solid">
+    <div>
+      <h2  className="text-black">{post.title}</h2>
+      <h2>{post.id}</h2>
+      <h2 className="text-red-600">{post.body}</h2>
+    </div>
+  </li>
+))
+
+return(
+        <div style={{backgroundImage: "url(/picture.webp)", backgroundSize:"cover", backgroundRepeat:"no-repeat", backgroundPosition: "center" }}> 
+        <h1 className="flex justify-center mb-2 text-2xl font-bold dark:text-white md:text-5xl lg:text-6xl">Posts</h1>
+        <ul>
+          {postsList}
+          
+        </ul>
+        </div>
+    )
+}
 /*Non riesco a mettere il bg diversamente...
 https://hygraph.com/blog/react-pagination
 https://www.youtube.com/watch?v=jmNHcW_oszg
